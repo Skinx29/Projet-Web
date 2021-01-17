cd7a1880c299224a594a0c47d6607da28e59486d
Skinx29 <77584756+Skinx29@users.noreply.github.com>
2021-01-17 18:53:09 +0100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,189 @@
+<?php
+	include 'fonctions.php';
+	$valeur= "Acceuil Site EAG";
+	EnteteTitrePage($valeur);
+?>
+
+
+
+
+
+
+	
+	<div class="container">
+		
+		
+		<div class="row">
+			
+			<div class="col-9">
+			<article>
+						<h3> <strong>Présentation de l'association: </strong> </h3>
+						<br>
+						L’En Avant de Guingamp est un club atypique. Un club détenu par 140 entreprises locales. Un club  auquel 400 bénévoles contribuent. Un club situé dans une ville de 7 000 habitants et dont le stade contient 18 000 places.
+						
+						Guingamp ce sont des succès sportifs éclatants : deux victoires en Coupe de France, trois montées en Ligue 1, un parcours exceptionnel en Europa League. C’est aussi et surtout une capacité à durer au plus haut niveau qui est un défi permanent. Deux fois descendu en national, le club est toujours parvenu à rejoindre immédiatement le monde pro.
+						
+						Pour obtenir ces résultats, le club ne dispose pas de moyens financiers comparables à ceux des places fortes du football français. Mais il sait fédérer, donner à tous l’envie de s’impliquer, en rassemblant tous ceux qui veulent relever le défi autour de valeurs qui sont le socle de l’identité du club.
+						
+						Ces valeurs sont le prolongement des principes humanistes des instituteurs qui ont fondé le club en 1912. Elles restent plus que jamais d’actualité :
+						
+						
+						<br>
+						<b><u>L’HUMILITE</u></b><br>
+						L’humilité est un préalable nécessaire à l’intégration dans le club. A Guingamp la seule star c’est le club.
+						<br>
+						<b><u>LE COMBAT</u></b><br>
+						Sans combat, point de salut. Notre dignité est dans la lutte constante que nous nous imposons tous. A Guingamp on ne nous a jamais rien donné, il a toujours fallu aller tout chercher en luttant avec acharnement.
+						<br>
+						<b><u>L’EXIGENCE</u></b><br>
+						Progresser, c’est un état d’esprit de chaque instant, c’est l’esprit En Avant. A Guingamp l’exigence, c’est renoncer une fois pour toute à la facilité.
+						<br>
+						<b><u>LA SOLIDARITE</u></b><br>
+						En Avant est une grande famille. Chaque jour, dirigeants, salariés, bénévoles, supporters, partenaires, entraîneurs, joueurs, avancent dans l’unité pour faire vivre collectivement un rêve commun. Etre Guingampais, c’est la jouer collectif pour vivre ensemble une formidable aventure humaine !
+						
+						
+					</article>
+				<div class="row">
+					
+					
+					
+					
+					<div class="col-5"><div id="myCarousel" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+							<li data-target="#myCarousel" data-slide-to="1"></li>
+							<li data-target="#myCarousel" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
+								<div class="container">
+									<div class="carousel-caption text-left">
+										<img src="medias/maillot.png" alt="Equipe féminine EAG " />
+									</div>
+								</div>
+							</div>
+							<div class="carousel-item">
+								<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
+								<div class="container">
+									<div class="carousel-caption">
+										<h4>Set de maillot de la saision 2020-2021.</h4>
+										<img src="medias/maillot2.png" alt="Equipe féminine EAG " />
+									</div>
+								</div>
+							</div>
+							<div class="carousel-item">
+								<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
+								<div class="container">
+									<div class="carousel-caption text-right">
+									<img src="medias/maillot3.png" alt="Equipe féminine EAG " />
+									</div>
+								</div>
+							</div>
+						</div>
+						<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+					
+					</div>
+					<div class="col-4"><iframe width="900" height="506" src="https://www.youtube.com/embed/KK69nZI-Ch8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						</div>
+				</div>
+			</div>
+			</div>
+			
+			
+			
+			
+			
+			<div class="col-3">
+				
+			<article>
+				<h5> Palmarès </h5>
+				<br>
+				<h4>COUPE DE FRANCE </h4>					<img src="medias/logo-fff.gif" alt="image fff" />
+				<div class="list-palm">
+					<strong>2008-2009 et 2013-2014</strong>
+					vainqueur
+				</div>
+				<div class="list-palm">
+					<strong>1996-1997</strong>
+					Finaliste
+				</div>
+				<div class="list-palm">
+					<strong>1997-1998,2014-2015 et 2016-2017</strong>
+					1/2 finaliste
+				</div>
+				<div class="list-palm">
+					<strong>1980-1981</strong>
+					1/4 finaliste
+				</div>
+				
+			<br>
+			
+			
+			<h4>EUROPA LEAGUE </h4> <img src="medias/logo-europa-league.gif" alt="image Europa League" />
+			<div class="list-palm">
+				<strong>2014-2015</strong>
+				16e de finale
+			</div>
+			
+			<br>
+			<h4>COUPE INTERTOTO </h4> <img src="medias/logo-intertoto.gif" alt="image Europa League" />
+			
+			<div class="list-palm">
+				<strong>2014-2015</strong>
+				16e de finale
+			</div>
+			
+			<br>
+			
+			<h4>COUPE DE LA LIGUE </h4><img src="medias/logo-coupe-ligue.gif" alt="image Europa League" />
+			<div class="list-palm">
+				<strong>2018-2019</strong>
+				Finaliste
+			</div>
+			<div class="list-palm">
+				<strong>1995-1996</strong>
+				1/2 finaliste
+			</div>
+			<div class="list-palm">
+				<strong>2009-2010 et 2015-2016</strong>
+				1/4 finaliste
+			</div>
+</article>
+
+<h5><b>Chant des membres de l'association</h5>
+<video controls>
+ <source src="medias/chant_eag.mp4" width=20% height=20% type="video/mp4">
+ Votre navigateur n'est pas compatible avec la balise
+<code>video</code>.
+</video>
+
+
+
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+	footer();
+	?>		
